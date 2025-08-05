0458cdc2a14130ac3f1e8ef9d9ce6ce6bcc567a9
alejandro sosa <alejandrososadev@gmail.com>
2025-08-05 18:36:21 -0400

    chore(app): read envs from env file adds @nestjs/config module (which reads envs)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
@@ -11,17 +11,22 @@ import { User } from './users/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'notifications',
-      entities: [User, Notification],
-      migrations: ['dist/src/migrations/*.js'],
-      synchronize: false,
-      migrationsRun: true, // run migrations automatically
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: parseInt(configService.get('POSTGRES_PORT', '5432'), 10),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [User, Notification],
+        migrations: ['dist/src/migrations/*.js'],
+        synchronize: false,
+        migrationsRun: true, // run migrations automatically
+      }),
+      inject: [ConfigService],
     }),
     NotificationsModule,
     UsersModule,
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -6,13 +6,22 @@ import { NotificationsController } from './notifications.controller';
 import { UsersModule } from '../users/users.module';
 import { BullModule } from '@nestjs/bull';
 import { NotificationsProcessor } from './notifications.processor';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Notification]),
     UsersModule,
-    BullModule.registerQueue({
+    BullModule.registerQueueAsync({
       name: 'notifications',
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: parseInt(configService.get('REDIS_PORT', '6379'), 10),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [NotificationsService, NotificationsProcessor],
